efd07b6e7a815023a24a696c7ed718f4db146a54
Thomas <thomas.m-tima@epitech.eu>
2022-12-19 12:48:00 -0500

    4 Finally Guill commit

---
+++ teg_auto/lib/classes/itemcard.dart
@@ -0,0 +1,20 @@
+class ItemCard {
+  const ItemCard({
+    required this.image,
+    required this.brand,
+    required this.model,
+    required this.price,
+    required this.km,
+    required this.color,
+    required this.state,
+    required this.details,
+  });
+  final String image;
+  final String brand;
+  final String model;
+  final String price;
+  final String km;
+  final String color;
+  final String state;
+  final String details;
+}
