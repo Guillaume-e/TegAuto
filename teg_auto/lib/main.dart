39ae1bcb8b0656f788a12656919e017e52f68e75
guillaume <guillaume.erembrom-poulle@epitech.eu>
2022-12-21 20:18:54 +0400

    iniatialise firebase ios, android, web, macos ok

--- teg_auto/lib/main.dart
+++ teg_auto/lib/main.dart
@@ -3,9 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:teg_auto/pages/admin_page.dart';
 import 'package:teg_auto/pages/home_page.dart';
 import 'package:teg_auto/pages/profil_page.dart';
+import 'firebase_options.dart';
+
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  
+
+WidgetsFlutterBinding.ensureInitialized();
+ await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+
+
   runApp(const MyApp());
 }
 
